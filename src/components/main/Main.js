19279a10a3ffec2eaa253ea014e3826478136f0a
Md Faridul islam <mdfaridulislamovi@gmail.com>
2023-03-31 04:29:51 +0600

    get data function added

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
 import Card from "../card/Card";
 import "./Main.style.css";
 
 export default function Main() {
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                setError(false);
+                setLoading(true);
+                const response = await fetch("../../../fakeData.json");
+                const data = await response.json();
+                setCardData(data);
+                setLoading(false);
+                console.log(data);
+            } catch (error) {
+                setError(true);
+                setLoading(false);
+                console.log(error);
+            }
+        };
+        getData();
+    }, []);
+
     return (
         <main id="main-sec">
             <div id="card-container">
-                <Card />
-                <Card />
-                <Card />
+                {loading && <h1>Loading...</h1>}
+                {!error &&
+                    !loading &&
+                    Array.isArray(cardData) &&
+                    cardData.length > 0 &&
+                    cardData.map((card) => <Card data={card} />)}
+                {error && <h2>There was an error to get data!</h2>}
             </div>
             <div id="right-sidebar">
                 <div id="spent-time">
